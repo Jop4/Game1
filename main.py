e79b06181e186b20b9b971ffd12184f84bc5fa12
Jop4 <Jop42019@yandex.ru>
2019-12-15 13:31:41 +1000

    'xbv,.?c;

--- main.py
+++ main.py
@@ -28,13 +28,17 @@ WIN_WIDTH, WIN_HEIGHT = 780, 630
 BG_COLOR = (192, 192, 192)
 BRICK_WIDTH = BRICK_HEIGHT = 30
 BRICK_COLOR = (0, 128, 0)
+FPS = 60
+clock = pygame.time.Clock()
+x1, y1 = WIN_WIDTH // 2, WIN_WIDTH // 2
+PLAYER_SIZE = 50, 50
 
 pygame.init()
 pygame.display.set_caption('первая игра')
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-brick = pygame.Surface((BRICK_WIDTH, BRICK_HEIGHT))
-brick.fill(BRICK_COLOR)
+player = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
+ # brick.fill(BRICK_COLOR)
 
 ran = True
 while ran:
@@ -49,9 +53,13 @@ while ran:
     for row in level:
         for col in row:
             if col == '-':
-                screen.blit(brick, (x, y))
+                screen.blit(player, (x1, y1))
+                brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
+                pygame.draw.rect(screen,(100, 0, 0), [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
             x += BRICK_WIDTH
         y += BRICK_HEIGHT
         x = 0
 
+    pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 1)}')
     pygame.display.update()
+    clock.tick(FPS)
